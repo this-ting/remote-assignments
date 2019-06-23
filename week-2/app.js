cadb8789e906150e287990ae9dd3a1036a0abc22
this.ting <46632205+this-ting@users.noreply.github.com>
2019-06-23 23:24:04 +0800

    Change text when click on Welcome banner

--- week-2/app.js
+++ week-2/app.js
@@ -4,4 +4,12 @@ const showBox = document.querySelector("div.section-show")
 
 button.addEventListener ('click', () => { 
     showBox.style.display = "contents";
+});
+
+// change text to "Have a Good Time!", when click on Welcome banner
+const clickBanner = document.querySelector("div.change-banner");
+const changeText = document.querySelector("h1.welcome-banner");
+
+clickBanner.addEventListener ('click', () => {
+    changeText.textContent = "Have a Good Time!";
 })
