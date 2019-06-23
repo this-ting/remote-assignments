794995b9001fd75c700e946f692c42f36b458c08
this.ting <46632205+this-ting@users.noreply.github.com>
2019-06-23 16:46:18 +0800

    Add assignment 2

---
+++ week-2/Assignment 2-Object.js
@@ -0,0 +1,60 @@
+/*
+Assignment 2: Object
+In JavaScript, there are many different sets of syntax for creating objects. In 
+the code below,function parameters are encapsulated into an object, try using 
+at least ​two ways​ to create a proper object as a parameter of the calculate 
+function.
+
+function calculate(args){
+    let result;
+    if(args.op==="+"){
+        result=args.n1+args.n2;
+    } else if(args.op==="-"){
+        result=args.n1-args.n2;
+    } else {
+        result="Not supported";
+}return result;
+}
+
+Try to call calculate function correctly
+For example, if we have an add function like this:
+    function add(args){
+        return args.n1+args.n2;
+    }
+    We can call it by passing an object created by JSON literal:
+        add({n1:3, n2:4});
+        And, you should find another way to create a proper object your way
+
+*/
+
+function calculate(args){
+    let result;
+    if(args.op==="+"){
+        result=args.n1+args.n2;
+    } else if(args.op==="-"){
+        result=args.n1-args.n2;
+    } else {
+        result="Not supported";
+} console.log(result);
+}
+
+
+// First way of calling via object in calculate function parameters
+
+calculate({
+    n1: 1,
+    n2: 2,
+    op: "-"
+})
+
+
+// Second way of calling via definging object variable
+var args1 = {
+    n1: 3,
+    n2: 4,
+    op: "+"
+};
+
+calculate(args1);
+
+
