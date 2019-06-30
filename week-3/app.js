1f808b9e634a59763ee005d58cea31fc18b58ec7
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-06-30 23:53:01 +0800

    Set up simple web sever & display welcome message

--- week-3/app.js
+++ week-3/app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
+// sends Hello message when launched
+app.get('/', (request, response) => {
+    response.send('Hello my server!');
+});
+
 app.listen(3000);
