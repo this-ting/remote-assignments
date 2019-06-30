7789f580b05b841800d6a5b620aa8a32cff897a5
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-01 00:03:12 +0800

    Add /getData

--- week-3/app.js
+++ week-3/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+//Assignment 1
 // sends Hello message when launched
 app.get('/', (request, response) => {
     response.send('Hello my server!');
+});
+
+//Assignment 2
+// When user enter ​http://localhost:3000/getData​ in a browser’s address bar, 
+// show "Lack of Parameter" message in the page.
+app.get('/getData', (request, response) => {
+    response.send('Lack of Parameter!');
 });
 
 app.listen(3000);
