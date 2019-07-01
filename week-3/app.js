3cf77f7fe59797929b316251aae11c2277d52053
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-01 12:47:46 +0800

    Set up view directory with pug templates

--- week-3/app.js
+++ week-3/app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
 
+// Tells express what template to use
+app.set('view engine', 'pug');
+
 //Assignment 1
 // sends Hello message when launched
-app.get('/', (request, response) => {
-    response.send('Hello my server!');
+app.get('/', (req, res) => {
+    res.send('<h1>Hello my server!</h1>');
 });
 
 //Assignment 2
-// When user enter ​http://localhost:3000/getData​ in a browser’s address bar, 
-// show "Lack of Parameter" message in the page.
-app.get('/getData', (request, response) => {
-    response.send('Lack of Parameter!');
+//  http://localhost:3000/getData​, show "Lack of Parameter" message in the page.
+app.get('/getData', (req, res) => {
+    res.send('Lack of Parameter!');
 });
 
-app.listen(3000);
+
+// sandbox
+app.get('/sandbox', (req, res) => {
+    res.render('main')
+})
+
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
