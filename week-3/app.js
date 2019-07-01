fe505b9a892d0094ea2cd754f9f72c57f7981fe0
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-02 01:14:08 +0800

    Set up route directory with index.js

--- week-3/app.js
+++ week-3/app.js
@@ -9,47 +9,11 @@ app.use(cookieParser());
 // Tells express what template to use
 app.set('view engine', 'pug');
 
-//Assignment 1
-// sends Hello message when launched
-app.get('/', (req, res) => {
-    res.send('<h1>Hello my server!</h1>');
-});
+// Importing from routes/index.js
+const routes = require('./routes');
 
-//Assignment 2
-//  http://localhost:3000/getData​, show "Lack of Parameter" message in the page.
-app.get('/getData', (req, res) => {
-    res.send('Lack of Parameter!');
-});
+app.use(routes);
 
-
-// Assignment 4: My Name (Cookies)
-app.get('/myName', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('myname', {name})
-    } else {
-        res.redirect('/trackName');
-    };
-});
-
-app.post('/myName', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.render('myname')
-});
-
-app.post('/trackName', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.render('trackname');
-});
-
-app.get('/trackName', (req, res) => {
-    res.render('trackname');
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username', '/myName');
-    res.redirect('/myName');
-});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000');
---
+++ week-3/routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Assignment 1
+// sends Hello message when launched
+router.get('/', (req, res) => {
+    res.send('<h1>Hello my server!</h1>');
+});
+
+//Assignment 2
+//  http://localhost:3000/getData​, show "Lack of Parameter" message in the page.
+router.get('/getData', (req, res) => {
+    res.send('Lack of Parameter!');
+});
+
+
+// Assignment 4: My Name (Cookies)
+router.get('/myName', (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.render('myname', {name})
+    } else {
+        res.redirect('/trackName');
+    };
+});
+
+router.post('/myName', (req, res) => {
+    const name = req.cookies.username;
+    res.cookie('username', req.body.username);
+    res.render('myname', {name})
+});
+
+router.post('/trackName', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.render('trackname');
+});
+
+router.get('/trackName', (req, res) => {
+    res.render('trackname');
+});
+
+router.post('/goodbye', (req, res) => {
+    res.clearCookie('username', '/myName');
+    res.redirect('/myName');
+});
+
+module.exports = router;
