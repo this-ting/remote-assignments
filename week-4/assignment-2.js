e3f4f69c23bb638748aa6be4a72464f2776d889c
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-02 17:50:12 +0800

    Add assignment 1 & 2

---
+++ week-4/assignment-2.js
@@ -0,0 +1,25 @@
+/*Assignment 2: Callback Function and Advanced HTML DOM
+Complete the functions below to make AJAX call to a URL by GET method, and show the response data in the page. You may render UI with any style.
+ 
+function ajax(src, callback){
+    // your code here
+}
+function render(data){
+    // your code here.
+    // ​document.createElement()​ and a​ ppendChild()​ methods are preferred. 
+}
+ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
+    render(response);
+}); ​// you should get product information in JSON format and render data in the page */
+
+
+function ajax(src, callback){
+    // your code here
+}
+function render(data){
+    // your code here.
+    // ​document.createElement()​ and a​ ppendChild()​ methods are preferred. 
+}
+ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
+    render(response);
+}); ​// you should get product information in JSON format and render data in the page
