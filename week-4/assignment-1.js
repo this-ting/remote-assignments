f0810fbbe8d05bdfee01b7babef661dc1eb3797e
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-05 16:11:14 +0800

    Complete assignment-1

--- week-4/assignment-1.js
+++ week-4/assignment-1.js
@@ -4,7 +4,6 @@ Complete the function below to show a delayed result in console.
 function delayedResult(n1, n2, delayTime, callback){
     // your code here
 }
-
 delayedResult(4, 5, 3000, function(result){
     console.log(result);
 }); ​// 9 (4+5) will be shown in the console after 3 seconds 
@@ -15,13 +14,14 @@ delayedResult(-5, 10, 2000, function(result){
 
 
 function delayedResult(n1, n2, delayTime, callback){
-    // your code here
+    let result = `${n1+n2} (${n1} + ${n2})`;
+    setTimeout(callback, delayTime,result); 
 }
 
 delayedResult(4, 5, 3000, function(result){
     console.log(result);
-}); ​// 9 (4+5) will be shown in the console after 3 seconds 
+}); 
 
 delayedResult(-5, 10, 2000, function(result){  
     window.alert(result);
-}); ​// 5 (-5+10) will be shown in an alert dialog after 2 seconds
+});
