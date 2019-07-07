bb64240a24aeb84d9a4faefdb432cb0ee04fcb6f
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-07 20:21:45 +0800

    Set up React Project

---
+++ week-4/assignment-3/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          THIS IS IT
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+
+
+
+
+export default App;
