deb217f090df41e26f744cc89ba06a02b7035495
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-07-08 08:50:30 +0800

    Complete assignment-2

--- week-4/assignment-2.js
+++ week-4/assignment-2.js
@@ -11,18 +11,34 @@ function render(data){
 }
 ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
     render(response);
-}); ​// you should get product information in JSON format and render data in the page */
+}); ​// you should get product information in JSON format and render data in the page 
+*/
 
 
 function ajax(src, callback){
-    xhttp.open("GET", src, true);
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+      if(xhttp.readyState === 4 && xhttp.status === 200){
+        let productList = JSON.parse(xhttp.responseText);
+        callback(productList);
+      };
+    };
+    xhttp.open("GET", src);
     xhttp.send();
-    console.log()
-}
+  };
+  
 function render(data){
-    // your code here.
-    // ​document.createElement()​ and a​ppendChild()​ methods are preferred. 
-}
+    let Table = document.createElement("table"); 
+    for(let i =0; i < data.length ; i++ ){
+        var addRow = Table.insertRow(data[i]);
+        addRow.insertCell(0).innerHTML = data[i].name;
+        addRow.insertCell(1).innerHTML = data[i].price;
+        addRow.insertCell(2).innerHTML = data[i].description;
+    };
+    let addDiv = document.getElementById("info");
+    addDiv.appendChild(Table);
+};
+  
 ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){ 
     render(response);
-}); ​// you should get product information in JSON format and render data in the page
+}); ​
